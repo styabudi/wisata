6054c903d40d65fae65ff0e22edfc0f118f4ad3f
Styabudi <aboutbudi@gmail.com>
2022-10-24 22:37:45 +0700

    add embed video

--- app/views/booking/detail.php
+++ app/views/booking/detail.php
@@ -5,6 +5,17 @@
             <div class="card-body">
                 <h5 class="card-title"><?= $data['wisata']['nama'] ?></h5>
                 <p class="card-text text-muted"><?= $data['wisata']['alamat'] ?></p>
+                <?php if ($data['wisata']['video'] != null && $data['wisata']['video'] != null) {
+                ?>
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="ratio ratio-16x9">
+                                <iframe src="<?= $data['wisata']['video'] ?>" title="YouTube video" allowfullscreen></iframe>
+                            </div>
+                        </div>
+                    </div>
+                <?php
+                } ?>
                 <p class="card-text"><?= $data['wisata']['catatan'] ?></p>
                 <p class="card-text text-muted">Harga tiket : Rp. <?= $data['wisata']['harga'] ?></p>
                 <div class="float-end">
